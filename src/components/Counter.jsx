9f6c58e5ea20f0a21ad6fa23a7c156d6411483bf
Tarun Mathur <tarun.mathur01@infosys.com>
2024-09-14 19:43:10 +0530

    X-ClassComp

---
+++ src/components/Counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize state
+    this.state = {
+      count: 0
+    };
+  }
+
+  // Method to increment the count
+  increment = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  // Method to decrement the count
+  decrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count - 1
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
